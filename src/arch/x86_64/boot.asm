4b78a2cb672955292194d316dd857ce36418a946
andrgolubev <andpgolubev@gmail.com>
2018-05-13 20:08:56 +0300

    Enable paging

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -9,8 +9,12 @@ start:
     call check_cpuid                    ; check that cpuid instruction is supported
     call check_long_mode                ; check if long mode can be used
 
+    call set_up_page_tables
+    call enable_paging
+
     mov dword [0xb8000], 0x2f4b2f4f     ; print 'OK' to screen
     hlt                                 ; halt the cpu
+
 
 check_multiboot:
     cmp eax, 0x36d76289                 ; check for magic value in `eax`
@@ -19,6 +23,7 @@ check_multiboot:
 .no_multiboot:
     mov al, "0"
     jmp error
+
 
 ; from OSDev Wiki:
 check_cpuid:
@@ -57,6 +62,7 @@ check_cpuid:
     mov al, "1"
     jmp error
 
+
 ; from OSDev Wiki:
 check_long_mode:
     ; test if extended processor info is available
@@ -78,6 +84,59 @@ check_long_mode:
     mov al, "2"
     jmp error
 
+
+set_up_page_tables:
+    ; map first P4 entry to P3 table
+    mov eax, p3_table
+    or eax, 0b11                        ; present + writable
+    mov [p4_table], eax
+
+    ; map first P3 entry to P2 table
+    mov eax, p2_table
+    or eax, 0b11                        ; present + writable
+    mov [p3_table], eax
+
+    ; map each P2 entry to a huge 2MiB page
+    mov ecx, 0                          ; counter variable
+
+.map_p2_table:
+    ; max ecx-th P2 entry to a huge page that starts at 2MiB*ecx
+    mov eax, 0x200000                   ; 2 MiB
+    mul ecx                             ; start address of ecx-th page
+    or eax, 0b10000011                  ; present + writable + huge
+    mov [p2_table + ecx*8], eax         ; map ecx-th entry | each entry is 8 bytes
+
+    inc ecx                             ; ecx++
+    cmp ecx, 512                        ; if ecx == 512: the whole P2 table is mapped
+    jne .map_p2_table                   ; else:          map next entry in P2 table
+
+    ret
+
+
+enable_paging:
+    ; load P4 to cr3 register (it is used by CPU to access P4)
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in EFER MSR
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
+
+
 ; Print 'ERR: ' and the given error code to screen and hang.
 ; parameter: error code (in ascii) in al
 error:
@@ -90,8 +149,16 @@ error:
     mov byte  [0xb800a], al             ; al - overrides last space with given error code
     hlt                                 ; halt the cpu
 
+
 ; reserve bytes for stack
 section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
 stack_bottom:
     resb 64
 stack_top:
